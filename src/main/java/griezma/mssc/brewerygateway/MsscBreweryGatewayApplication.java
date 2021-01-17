b274cc8e9cc2b888634256d42fe9841caeb2f0ab
griezma <griezma@outlook.com>
2021-01-17 12:36:12 +0100

    working sample

--- src/main/java/griezma/mssc/brewerygateway/MsscBreweryGatewayApplication.java
+++ src/main/java/griezma/mssc/brewerygateway/MsscBreweryGatewayApplication.java
@@ -1,7 +1,10 @@
-package griessma.mssc.brewerygateway;
+package griezma.mssc.brewerygateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 
 @SpringBootApplication
 public class MsscBreweryGatewayApplication {
@@ -9,5 +12,13 @@ public class MsscBreweryGatewayApplication {
     public static void main(String[] args) {
         SpringApplication.run(MsscBreweryGatewayApplication.class, args);
     }
-
 }
+
+@RestController
+class Fallback {
+    @GetMapping("/fallback")
+    String fallback() {
+        return "fallback called";
+    }
+}
+
---
+++ src/test/java/griezma/mssc/brewerygateway/GatewayTests.java
@@ -0,0 +1,31 @@
+package griezma.mssc.brewerygateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+@ActiveProfiles("sample")
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+//@AutoConfigureWireMock(port = 0)
+class GatewayTests {
+
+    @Autowired
+    WebTestClient client;
+
+    @Test
+    void contextLoads() {
+//        stubFor(get(urlEqualTo("/mirror/get"))
+//                .willReturn(aResponse()
+//                    .withHeader("Content-Type", "application/json")
+//                    .withBody("{\"headers\":{\"X-Hello\":\"World\"}")));
+
+        client.get().uri("/mirror/get").exchange()
+                .expectStatus().isOk()
+                .expectBody()
+                    .jsonPath("$.headers.Forwarded").exists()
+                    .jsonPath("$.headers.Host").isEqualTo("httpbin.org");
+    }
+
+}
